430adc59651d763cdae64bbe4c8ad6d82c5128b2
Majedul Hasan <hasanmajedul@gmail.com>
2023-05-02 01:05:25 +0600

    region list chef recipe list, by area

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5200
 app.use(cors())
-const chefsProfile = require('./data/chefsProfile.json')
+const chefsProfile = require('./data/chefsProfile.json');
+const recipes = require('./data/recipes.json');
+
 
 
 
@@ -14,6 +16,59 @@ app.get('/', (req, res) => {
 })
 app.get('/chefsProfile', (req, res) => {
   res.json(chefsProfile)
+})
+app.get('/recipes', (req, res) => {
+  res.json(recipes)
+})
+app.get('/recipes/region', (req, res) => {
+    const regions = recipes.map(x=>x.strArea)
+   const regionList = regions.filter((value, index, array) => array.indexOf(value) === index)
+
+
+  res.json(regionList)
+})
+app.get('/recipes/:chef_id', (req, res) => {
+    const {chef_id} = req.params
+    const chefsResList = chefsProfile.find(x=>x._id ===chef_id)
+    if(chefsResList){
+        
+      const x =  chefsResList.recipes.map(x=>{
+        let newMeal = {}
+        recipes.forEach(y=>{
+        if(y.idMeal === x){
+            const strIngredients = {};
+            for (let i = 1; i <= 20; i++) {
+                const ingredient = y[`strIngredient${i}`];
+                const measure = y[`strMeasure${i}`];
+                if (ingredient && measure) {
+                    strIngredients[ingredient] = measure;
+                }
+            }
+            console.log(strIngredients);
+             newMeal = {
+                ...y,
+                strIngredients: strIngredients,
+            };
+        } 
+      })
+      return newMeal
+    }
+     
+      )
+
+      chefsResList.recipes = x;
+      res.json(chefsResList);
+    } else  res.send('recipe not Found')
+})
+app.get('/foods/:origin', (req, res) => {
+    const {origin} = req.params
+    console.log(origin);
+    const recipeList = recipes.filter(x=>x.strArea.toLowerCase() === origin.toLowerCase())
+    if(recipeList){      
+        res.send(recipeList.map(x=>x.idMeal))
+    } else {
+        res.send('recipe not Found')
+    }  
 })
 
 app.listen(port, () => {
---
+++ texds.js
@@ -0,0 +1,41 @@
+const meal = {
+    "idMeal": "52893",
+    "strMeal": "Apple & Blackberry Crumble",
+    "strDrinkAlternate": null,
+    "strCategory": "Dessert",
+    "strArea": "British",
+    "strMealThumb": "https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg",
+    "strTags": "Pudding",
+    "strYoutube": "https://www.youtube.com/watch?v=4vhcOwVBDO4",
+    "strIngredient1": "Plain Flour",
+    "strIngredient2": "Caster Sugar",
+    "strIngredient18": "",
+    "strIngredient19": "",
+    "strIngredient20": "",
+    "strMeasure1": "120g",
+    "strMeasure2": "60g",
+    "strMeasure18": "",
+    "strMeasure19": "",
+    "strMeasure20": "",
+    "strSource": "https://www.bbcgoodfood.com/recipes/778642/apple-and-blackberry-crumble"
+  };
+  
+  const strIngredients = {};
+  
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = meal[`strIngredient${i}`];
+    const measure = meal[`strMeasure${i}`];
+    
+    if (ingredient && measure) {
+      strIngredients[ingredient] = measure;
+    }
+  }
+  
+  const newMeal = {
+    ...meal,
+    strIngredients,
+  };
+  
+  
+  
+  
