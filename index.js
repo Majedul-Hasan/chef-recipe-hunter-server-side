9fe7284f4dcd130aaca825e736bc112a95b175b6
Majedul Hasan <hasanmajedul@gmail.com>
2023-05-01 23:03:01 +0600

    data recipes

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5200
+app.use(cors())
+const chefsProfile = require('./data/chefsProfile.json')
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/chefsProfile', (req, res) => {
+  res.json(chefsProfile)
 })
 
 app.listen(port, () => {
